5610f1dc536092aae49b0ca1fb2e18c42aaca840
Faraz Fazli <farazfazli@gmail.com>
2016-11-16 00:35:02 -0500

    Update code to output individual files, improve image output

--- moodanalysis.r
+++ moodanalysis.r
@@ -1,18 +1,22 @@
 require("rtweet")
 require("wordcloud")
 
-twitter_token <- create_token(app = "APP_NAME_HERE",
-                              consumer_key = "CONSUMER_KEY_HERE",
-                              consumer_secret = "CONSUMER_SECRET_HERE")
+twitter_token <- create_token(app = "APP_NAME",
+                              consumer_key = "CONSUMER_KEY",
+                              consumer_secret = "CONSUMER_SECRET")
 
 happy <- search_tweets("happy", n = 10000, token = twitter_token, lang = "en")
 sad <- search_tweets("sad", n = 10000, token = twitter_token, lang = "en")
-
-generateCloud(happy)
-generateCloud(sad)
 
 generateCloud<-function(tweets) {
   tweetsVector = unlist(tweets)
   tweetFrequency = table(tweetsVector)
   wordcloud(names(tweetFrequency), min.freq = 10, random.order = FALSE, colors = palette())
 }
+
+png("happy.png", width=12, height=8, units="in", res=600)
+generateCloud(happy)
+dev.off()
+png("sad.png", width=12, height=8, units="in", res=600)
+generateCloud(sad)
+dev.off()
